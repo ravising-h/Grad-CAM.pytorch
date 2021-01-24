5753feee9ec4cedb423cfa667c618f1088df9f43
RAVI SINGH <43233175+ravising-h@users.noreply.github.com>
2021-01-25 00:06:45 +0530

    Update grad_cam.py

--- detection/grad_cam.py
+++ detection/grad_cam.py
@@ -96,7 +96,7 @@ class GradCamPlusPlus(GradCAM):
         :return:
         """
         self.net.zero_grad()
-        output = self.net.inference([inputs])
+        output = self.net([inputs])
         print(output)
         score = output[0]['instances'].scores[index]
         proposal_idx = output[0]['instances'].indices[index]  # box来自第几个proposal
